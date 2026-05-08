b0b28f8e81791cf2a98b24eb9652e4d60617fd2e
Nadiaiscute2016 <ojinadiaoa@gmail.com>
2026-05-08 19:02:16 +0200

    Yay

---
+++ Matplotlib.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+months = ["Jan", "Feb", "Mar", "Apr", "May"]
+sales = [120, 150, 180, 200, 170]
+
+plt.plot(months, sales, marker='o')
+
+plt.title("Monthly Sales")
+plt.xlabel("Months")
+plt.ylabel("Sales")
+
+plt.show()
